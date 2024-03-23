c4baa4a877e3e168bf3a34e358e3c85eb1f56f6b
nanpangyou <yigehd@foxmail.com>
2024-03-23 21:21:07 +0800

    js pragma

--- Day1/react-runner/app.jsx
+++ Day1/react-runner/app.jsx
@@ -1,3 +1,5 @@
+// /**@jsx myReact.createElement */
+// 上面这行叫pragma，是babel的一个特性，用来告诉babel如何解析jsx语法
 import { React } from "./core/React.js";
 // export const App = React.createElement("div", {}, "appV3", "hello");
 
